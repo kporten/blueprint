e2037481c762fb61b138746c4415cbe15e91307e
Kevin Porten <hello@kevinporten.dev>
2025-03-25 21:25:50 +0100

    replace uuid with cuid2

--- apps/with-hono/src/db/schema.ts
+++ apps/with-hono/src/db/schema.ts
@@ -1,8 +1,11 @@
-import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { createId } from '@paralleldrive/cuid2';
+import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
 import { createSelectSchema } from 'drizzle-zod';
 
 export const taskTable = pgTable('task', {
-  id: uuid().primaryKey().defaultRandom(),
+  id: text()
+    .primaryKey()
+    .$default(() => createId()),
   description: text().notNull(),
   createdAt: timestamp().notNull().defaultNow(),
   updatedAt: timestamp()
--- apps/with-hono/src/db/seed.ts
+++ apps/with-hono/src/db/seed.ts
@@ -1,17 +1,21 @@
 import { reset, seed } from 'drizzle-seed';
 
 import { db } from '#db/client';
-import * as schema from '#db/schema';
+import { taskTable } from '#db/schema';
 
-await reset(db, schema);
+await reset(db, { taskTable });
 
-await seed(db, schema, {
-  seed: 1,
-}).refine((f) => ({
+await seed(
+  db,
+  { taskTable },
+  {
+    seed: 1,
+  },
+).refine((f) => ({
   taskTable: {
+    count: 100,
     columns: {
       description: f.loremIpsum({ sentencesCount: 5 }),
     },
-    count: 100,
   },
 }));
